14f266957e4305dbc28987a9abb2860a67ffde2c
mweaver20 <morganweaver123@yahoo.com>
2022-11-19 12:27:15 -0800

    fixing Track and tracklist comp

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -11,14 +11,16 @@ class Track extends React.Component {
     }
   }
 
-  redner() {
+  render() {
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3> track name will go here </h3>
-          <p>track artist will go here | track album will go here </p>
+          <h3> {this.props.track.name} </h3>
+          <p>
+            {this.props.track.artist} | {this.props.track.album}
+          </p>
         </div>
-        {this.renderAction}
+        {this.renderAction()}
       </div>
     )
   }
