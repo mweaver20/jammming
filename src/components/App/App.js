6178c2d06d76b40e533af9e4db4ad6c49a475944
mweaver20 <morganweaver123@yahoo.com>
2022-11-21 15:02:54 -0800

    reseting playlist state after saving playlist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -46,15 +46,12 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map((track) => track.uri)
-    Spotify.savePlaylist(this.state.playlistName, trackURIs)
-    /*
-    .then(() => {
+    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
       return this.setState({
         playlistName: 'New Playlist',
         playlistTracks: [],
       })
     })
-    */
   }
 
   search(term) {
