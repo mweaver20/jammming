06a47ce2b9ae9bcb9efda717689031a101367317
mweaver20 <morganweaver123@yahoo.com>
2022-11-20 17:39:08 -0800

    fixing search function

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,11 +5,9 @@ import './SearchBar.css'
 class SearchBar extends React.Component {
   constructor(props) {
     super(props)
-
     this.state = {
       term: '',
     }
-
     this.search = this.search.bind(this)
     this.handleTermChange = this.handleTermChange.bind(this)
   }
@@ -29,7 +27,7 @@ class SearchBar extends React.Component {
           placeholder="Enter A Song, Album, or Artist"
           onChange={this.handleTermChange}
         />
-        <button className="SearchButton" onClick={this.Search}>
+        <button className="SearchButton" onClick={this.search}>
           SEARCH
         </button>
       </div>
